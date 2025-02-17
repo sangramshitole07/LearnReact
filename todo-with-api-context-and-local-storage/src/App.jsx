dfe5dcba3ea93c6da974e029d297e81f2366f445
Sangram Shitole <skshitole10@gmail.com>
2025-02-17 17:49:31 +0530

    Added  Context

---
+++ todo-with-api-context-and-local-storage/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h2 class="text-blue-500 hover:text-blue-100">Hello world</h2>
+    </>
+  )
+}
+
+export default App
